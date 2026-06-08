a27ced2e153c44464c1e79209cd0556cd1600224
scherbaksergii-ops <scherbak.sergii@eu.edu.ua>
2026-06-08 07:03:49 +0300

    Add Rust practice1 project

---
+++ practice1/target/debug/deps/practice1.d
@@ -0,0 +1,5 @@
+C:\Users\Сергiй\rust-labs12\practice1\target\debug\deps\practice1.d: src\main.rs
+
+C:\Users\Сергiй\rust-labs12\practice1\target\debug\deps\practice1.exe: src\main.rs
+
+src\main.rs:
---
+++ practice1/target/debug/practice1.d
@@ -0,0 +1,1 @@
+C:\Users\Сергiй\rust-labs12\practice1\target\debug\practice1.exe: C:\Users\Сергiй\rust-labs12\practice1\src\main.rs
