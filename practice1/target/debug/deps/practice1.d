1f50e2283b3e8541a41e844075844bd220e1a737
scherbaksergii-ops <scherbak.sergii@eu.edu.ua>
2026-06-08 07:05:58 +0300

    Remove build files and add gitignore

--- practice1/target/debug/deps/practice1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Сергiй\rust-labs12\practice1\target\debug\deps\practice1.d: src\main.rs
-
-C:\Users\Сергiй\rust-labs12\practice1\target\debug\deps\practice1.exe: src\main.rs
-
-src\main.rs:
--- practice1/target/debug/practice1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Сергiй\rust-labs12\practice1\target\debug\practice1.exe: C:\Users\Сергiй\rust-labs12\practice1\src\main.rs
