4cb1a64f4b69818299702c3275d73d4eda6279f0
scherbaksergii-ops <scherbak.sergii@eu.edu.ua>
2026-06-08 09:07:10 +0300

    Move screenshot from practice1 to practice2

--- practice1/src/main.rs
+++ practice1/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+fn staircase(n: usize) -> Vec<String> {
+    let mut res = vec![];
+
+    for i in 1..=n {
+        res.push(format!("{}{}", " ".repeat(n - i), "#".repeat(i)));
+    }
+
+    res
+}
+
+#[test]
+fn test_staircase() {
+    assert_eq!(staircase(3), vec![
+        "  #",
+        " ##",
+        "###",
+    ]);
 }
